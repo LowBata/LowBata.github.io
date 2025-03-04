8010ec6e5463f43ab413608b367951316ec42af9
Lowliet <lowdata114@gmail.com>
2025-03-03 16:39:38 -0800

    To The Top Button

---
+++ Top-Button-JS.js
@@ -0,0 +1,17 @@
+﻿// Get the button
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Show the button when the user scrolls down 200px
+window.onscroll = function () {
+    if (document.body.scrollTop > 175 || document.documentElement.scrollTop > 175) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+};
+
+// Scroll to the top of the page when the button is clicked
+scrollToTopBtn.addEventListener("click", function () {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+});
